d84c60021cd5ddcc1f24f8bc9014d6dcb112ac30
SaihttamR <matthias@regtteor.de>
2022-01-28 12:01:57 +0100

    Hide download button in index

--- lib/view_hook_listener.rb
+++ lib/view_hook_listener.rb
@@ -1,4 +1,6 @@
 class ViewHookListener < Redmine::Hook::ViewListener
-  render_on :view_issues_show_details_bottom, :partial => 'issue_release_note/button_to_release_note'
+  render_on :view_issues_show_details_bottom,
+            :partial => 'issue_release_note/button_to_release_note',
+            locals: { force_button: true, missing_hint_i18n_key: 'missing_release_note' }
   render_on :view_issues_sidebar_issues_bottom, :partial => 'issue_release_note/link_to_index'
 end
